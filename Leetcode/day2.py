dce794f0af3e4fb5663fbdad93e021debfde8607
snipnithish <nithishganesh5845@gmail.com>
2026-02-17 15:57:46 +0530

    Add files via upload

---
+++ Leetcode/day2.py
@@ -0,0 +1,71 @@
+# type_student=(input("enter the student type (MGSH/MGSDS)="))
+# tuition_fee=float(input("enter the tuition fee="))
+# bus_fee=float(input("enter the bus fee="))
+# hostel_fee=float(input("enter the hostel fee="))
+# if type_student=="MGSDS":
+#     total_fee=(tuition_fee*1.5) + bus_fee
+#     print('total amount of MGSDS fee',total_fee)
+# elif type_student=="MGSH":
+#     total_fee=(tuition_fee*1.5) + hostel_fee
+#     print('total amount of MGSH fee',total_fee)
+# else:
+#     print("invalid type of student:please enter the student type")
+
+# limit=10000
+# account_balance=float(input("enter the account balance"))
+# with_drawal=float(input("enter the with drawal amount"))
+# if with_drawal>account_balance:
+#     print("isufficient balance")
+# elif with_drawal>limit:
+#     print("withdrawal amount exceed to limit")
+# else:
+#     print("error")
+
+# account_balance = 10000
+
+# ATM_Pin = int(input("Enter the PIN = "))
+# if ATM_Pin == 2005:
+#     print("continue")
+#     withdrawel_amount = float(input("Enter the withdrawel amount : "))
+#     if (withdrawel_amount > account_balance):
+#         print("Insufficient funds or Wrong input")
+#     elif withdrawel_amount < 0:
+#         print("invalid amount")
+#     elif withdrawel_amount==0 :
+#         print("invalid amount")
+#     else :
+#         print("withdraw Successfull = ",withdrawel_amount)
+#         print("Account balance =",account_balance - withdrawel_amount)
+# else :
+#     print("Wrong PIN") 
+
+
+# age=int(input("enter the age:"))
+# showtime=(input("SHOWTIME morning/evenging:"))
+# if(age<5):
+#     print("free entery")
+# elif(age<17):
+#     print("child ticket")
+#     price=150
+#     if showtime=="morning":
+#         print("ticket price",price-50)
+#     else:
+#         print("ticket price:",price)
+# elif(age<60):
+#     print("adult ticket")
+#     price=250
+#     if showtime=="morning":
+#         print("ticket price",price-50)
+#     else:
+#         print("ticket price:",price)
+# else:
+#     print("senior citizen")
+#     if showtime=="morning":
+#         print("ticket price",150)
+#     else:
+#         print("ticket price:",200)
+
+
+
+
+
---
+++ Leetcode/day2loops.py
@@ -0,0 +1,71 @@
+# sum=0
+# for i in range(1,101):
+#     if i%2!=0:
+#         print(i)
+#         sum=sum+i
+#         print(sum)
+
+# sum=0
+# for i in range(1,101):
+#     if i % 2==0:
+#         print(i)
+#         sum=sum+i
+# print(sum)
+
+# sum=0
+# for i in range(1,11):
+#     sum=sum+5
+#     print(i,'x','5','=',sum)
+
+# for i in range(1,6):
+#     print('*'*i)
+
+# for i in range(1,6):
+#     i=(6-i)
+#     print('*'*i)
+
+# sum=0
+# i=1
+# while i<=100:
+#     if i%2!=0:
+#         print(i)
+#         sum=sum+i
+#     i=i+1
+# print(sum)
+
+# sum=0
+# i=1
+# while i<=100:
+#     if i % 2==0:
+#         print(i)
+#         sum=sum+i
+#     i=i+1
+# print(sum)
+
+# sum=0
+# i=1
+# while i<=10:
+#     sum=sum+5
+#     print(i,'x','5','=',sum)
+#     i=i+1
+
+# i=1
+# while i<=5:
+#     print('*'*i)
+#     i=i+1
+
+# i=1
+# while i<=5:
+#     print('*'*(6-i))
+#     i=i+1
+total_seats=10
+while total_seats>0:
+    name=input("enter the name of the passenger:")
+    print("welcome",name)
+    total_seats=total_seats-1
+    print("seats",total_seats+1,"booked for",name)
+    print("available seats:",total_seats)
+
+
+
+
