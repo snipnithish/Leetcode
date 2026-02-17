3163c13e7c25677fcc2855dc52be0dffb404d0fe
snipnithish <nithishganesh5845@gmail.com>
2026-02-17 10:07:36 +0530

    Add files via upload

---
+++ Leetcode/demo.py
@@ -0,0 +1,79 @@
+# print('hello')
+# apple123=10
+# print(apple123)
+
+# datatypes
+# 1.int
+# 2.float
+# 3.boolean
+# 4.str
+
+# age=int(input("enter the birth="))
+# future=2026
+# current=future-age
+# print(current)
+
+# current_age=20
+# maximum_age=50
+# per_day=5
+# left_years=50-20
+# years=(left_years*365)
+# total=(years*per_day)
+# print("you need",total)
+
+# trip_distance=1000
+# car_mileage=20
+# petrol_price=95
+# fuel_need=trip_distance/car_mileage
+# total=(fuel_need*petrol_price)
+# print("total cast",total)
+
+# daily_usage=1.5
+# total_days=30
+# total_usage=(total_days*daily_usage)
+# print("total",total_usage)
+
+# current_age=20
+# convert_years=20*365
+# print(convert_years)
+
+# original_price=1000
+# discount_percentage=10
+# discount_amount=(discount_percentage/100)*original_price
+# final_amount=original_price-discount_amount
+# print("total amount",final_amount)
+
+# x=6
+# y=5
+# if (x>y):
+#     print(x,'is greater than ',y)
+# elif(x<y):
+#     print(x,' is less than ',y)
+# else:
+#     print(x,'is equal to ',y)
+
+# mark=int(input("enter the marks="))
+# if mark >=0 and mark <=100:
+#     if mark==100:
+#         print("grade=s")
+#     elif mark >=90:
+#         print("grade=A")
+#     elif mark >=80:
+#         print("grade=B")
+#     elif mark >=70:
+#         print("grade=c")
+#     elif mark >=60:
+#         print("grade=D")
+#     else:
+#         print("invalid number")
+
+# x=float(input("enter the total banana dozen cost="))
+# y=float(input("enter the sold cost of banana="))
+# profit=(y*12)-x
+# if profit > x:
+#     print('profit rs',profit)
+# elif profit < x:
+#     print("loss rs",profit)
+# else:
+#     print('no loss no profit')
+
